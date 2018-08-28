a2428fc75f9848041f5eebc99da3720648187118
Matheus Coelho Espinasso <matheus.espinasso@gmail.com>
2018-08-27 21:13:51 -0300

    Aplicação dinâmica de constraints de layout

--- TCS-Teste/RegistrationViewController.swift
+++ TCS-Teste/RegistrationViewController.swift
@@ -23,49 +23,63 @@ class RegistrationViewController: UIViewController {
             DispatchQueue.main.async {
                 
                 if let regFormFields = regFormfields {
-                    // If there's no previous, the main view is the baseline
-                    var previousField: UIView = self.view
-                    
                     for field in regFormFields {
-                        var heightOffset: CGFloat = 30
-                        
-                        if (!previousField.isEqual(self.view)) {
-                            heightOffset = previousField.bounds.height
-                        }
-                        
-                        let fieldComp = self.createUIElement(for: field, constraintBaseline: previousField)
+                        let fieldComp = self.createUIElement(for: field)
                         fieldComp.isHidden = field.hidden
                         
                         self.view.addSubview(fieldComp)
                         fieldComp.translatesAutoresizingMaskIntoConstraints = false
                         
-                        if (field.type == .checkbox) {
-                            let label = UILabel()
-                            label.text = field.message
-                            
-                            self.view.addSubview(label)
-                            label.translatesAutoresizingMaskIntoConstraints = false
-                            
-                            label.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
-                            label.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16).isActive = true
-                            
-                            fieldComp.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
-                            fieldComp.leftAnchor.constraint(equalTo: label.rightAnchor, constant: 16).isActive = true
-                            fieldComp.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
-                            
-                            
-                        } else {
-                            fieldComp.bounds.size.width = self.view.bounds.size.width - 32
-                            fieldComp.bounds.size.height = 30
-                            fieldComp.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
-                            fieldComp.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16).isActive = true
-                            fieldComp.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
-                        }
-                        
                         self.fieldset.append((field: field, fieldComponent: fieldComp))
-                        previousField = fieldComp
                     }
+                    
+                    self.setLayoutConstraints()
                 }
+            }
+        }
+    }
+    
+    
+    func setLayoutConstraints() {
+        // If there's no previous, the main view is the baseline for constraints
+        var previousField: UIView = self.view
+        
+        for fieldTuple in fieldset {
+            var heightOffset: CGFloat = 25
+            let field = fieldTuple.field
+            let fieldComponent = fieldTuple.fieldComponent
+            
+            fieldComponent.removeFromSuperview()
+            self.view.addSubview(fieldComponent)
+            
+            if (!previousField.isEqual(self.view)) {
+                heightOffset = previousField.bounds.height
+            }
+            
+            if (field.type == .checkbox) {
+                let switchLabel = UILabel()
+                switchLabel.text = field.message
+                
+                self.view.addSubview(switchLabel)
+                switchLabel.translatesAutoresizingMaskIntoConstraints = false
+                
+                switchLabel.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
+                switchLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16).isActive = true
+                
+                fieldComponent.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
+                fieldComponent.leftAnchor.constraint(equalTo: switchLabel.rightAnchor, constant: 16).isActive = true
+                fieldComponent.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
+                
+            } else {
+                fieldComponent.bounds.size.width = self.view.bounds.size.width - 32
+                fieldComponent.bounds.size.height = 30
+                fieldComponent.topAnchor.constraint(equalTo: previousField.topAnchor, constant: CGFloat(field.topSpacing) + heightOffset).isActive = true
+                fieldComponent.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16).isActive = true
+                fieldComponent.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
+            }
+            
+            if !fieldComponent.isHidden {
+                previousField = fieldComponent
             }
         }
     }
@@ -90,11 +104,12 @@ class RegistrationViewController: UIViewController {
         for fieldTuple in fieldset {
             if fieldTuple.field.id == id {
                 fieldTuple.fieldComponent.isHidden = !show
+                setLayoutConstraints()
             }
         }
     }
     
-    func createUIElement(for field: RegistrationForm, constraintBaseline: UIView) -> UIView {
+    func createUIElement(for field: RegistrationForm) -> UIView {
         let fieldComp: UIView
         
         switch field.type {
@@ -145,8 +160,6 @@ class RegistrationViewController: UIViewController {
             
             let valueSwitch = UISwitch()
             valueSwitch.addTarget(self, action: #selector(switchValueDidChange(_:)), for: .valueChanged)
-            
-            
             
             fieldComp = valueSwitch
             
